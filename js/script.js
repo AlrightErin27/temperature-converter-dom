ef24bcb45d30fe9ef09102eeab421cfff423c8ba
AlrightErin27 <erinmontybruce@gmail.com>
2021-01-28 19:02:31 -0800

    css is done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('Hello, front end');
+
+// document.querySelector(".submitButton").addEventListener("click" , function(){
+//     const newText = document.querySelector("#twood-input").value
+//     document.querySelector(".twood").innerText = newText
+//     document.querySelector("#twood-input").value = ""
+
+
+
+function mySubmit() {
+  document.getElementById("myForm").submit()
+}
+// function myClear() {
+
+// }
